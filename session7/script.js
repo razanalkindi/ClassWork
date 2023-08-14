5a6fb82b7f6e3e201d43302079beba94a584dd82
razanalkindi <141748634+razanalkindi@users.noreply.github.com>
2023-08-14 19:04:48 +0400

    Add files via upload

---
+++ session7/script.js
@@ -0,0 +1,28 @@
+// let element = document.getElementsByName('div')
+// console.log(element)
+
+// let element = document.getElementsById('x')
+// let text = "<h1>Razan</h1>"
+// element.innerHTML="text"
+// console.log(element)
+
+// let text = document.getElementById('text')
+// text.innerHTML="Razan <br>"
+// text.innerHTML+=" Moosa <br>"
+// text.innerHTML+="Alkindi"
+
+// let btn=document.getElementById('btn')
+// btn.addEventListener('click' ,function(
+//     {
+//         let inp1
+
+
+//     }))
+
+
+// let btn=document.getElementById('btn')
+// btn.addEventListener('click' , function(){
+// let box=document.getElementsByClassName(' box')[0]
+// box
+// })
+
