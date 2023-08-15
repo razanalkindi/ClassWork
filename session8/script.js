4fc67a0623499ce1dca54ba62537573a59aef262
razanalkindi <141748634+razanalkindi@users.noreply.github.com>
2023-08-15 19:23:07 +0400

    Add files via upload

---
+++ session8/script.js
@@ -0,0 +1,63 @@
+// setTimeout(()=>{
+//     setTimeout(()=>{
+//         console.log("Welcome")
+//     },3000)
+// },3000)
+
+// let box = document.getElementById('box')
+// let btn = document.getElementById(' btn')
+// // let x = setTimeout(()=>{
+// //     box.style.display='block'
+// // },3000)
+
+// console.log(x);
+// btn.addEventListener('click' ,()=>{
+//     clearInterval(x)
+// })
+
+// let x =setInterval(() => {
+//     console.log("WELCOME RAZAN !!");
+// } ,100);
+
+// let box1 = document.getElementById("box1");
+// let box2 = document.getElementById("box2");
+// let box3 = document.getElementById("box3");
+
+// setTimeout(()=>{
+//     box1.style.display='block'
+
+// },3000)
+
+// setTimeout(()=>{
+//     box2.style.display='block'
+
+// },4000)
+
+// setTimeout(()=>{
+//     box3.style.display='block'
+
+// },5000)
+
+
+
+
+
+
+ 
+// let main=document.getElementById('main')
+// let group=document.getElementsByTagName('li')
+// if(localStorage.getItem('colors') !==unll)
+// {
+//     let y =JSON.parse(localStorage.getItem("colors"));
+//     main.style.background=y
+// }
+// for(let i = 0; i<group.length ; i++){
+//     group[i].addEventListener('click' ,function(){
+//         let x =group[i].getAnimations('liColor')
+//         main.style.background=x
+//         localStorage.setItem('colors' , JSON.stringify(x))
+//     })
+// }
+
+
+
