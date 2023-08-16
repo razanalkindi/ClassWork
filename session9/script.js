95d2cd1a92bd11e9cf229068157338ba6252fda7
razanalkindi <141748634+razanalkindi@users.noreply.github.com>
2023-08-16 19:14:22 +0400

    Add files via upload

---
+++ session9/script.js
@@ -0,0 +1,57 @@
+// let element = document.createElement('div')
+// element.contains.add('box')
+// element.setAttribute('clase' , 'box')
+// element.setAttribute('id' , 'x')
+// document.body.appendChild(element)
+
+// let divElement=document.createElement('dir')
+// divElement.setAttribute('id','container')
+// divElement.classList('container')
+
+
+
+// let ProductName=document.getElementById("ProductName")
+// let ProductPrice=document.getElementById("ProductPrice")
+// let ProductCategory=document.getElementById("ProductCategory")
+// let Products=document.getElementById("Products")
+// let btn=document.getElementById("btn")
+// let arrproduct=[]
+
+// btn.addEventListener("click" ,()=>{
+//     let prodata={
+//         pName:ProductName.value,
+//         pPrice:ProductPrice.value,
+//         pCato:ProductCategory.value,
+//         pdisc:Products.value,
+//     }
+//     arrproduct.push(prodata)
+//     loopdata();
+//     cleardata();
+// })
+
+// function loopdata(){
+//     let tabledata=''
+//     let x=0
+//     for(i=0; i<arrproduct; i++){
+//         tabledata+= `
+//         <tr>
+//         <td>${++x}</td>
+//         <td>${arrproduct[i].pName}</td>
+//         <td>${arrproduct[i].pPrice}</td>
+//         <td>${arrproduct[i].pCato}</td>
+//         <td>${arrproduct[i].pCato}</td>
+//         <td>${arrproduct[i].pdisc}</td>
+//         <tb><button id="dl">Delete</button></td>
+//         <tb><button id="update">Update</button></td>
+        
+//         </tr>`
+//     }
+//     document.getElementById("info").innerHTML=tabledata
+// }
+
+// function cleardata(){}
+
+
+
+
+
