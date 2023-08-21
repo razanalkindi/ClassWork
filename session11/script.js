34057f118679b618966cd52fc9246d56d6a413ed
razanalkindi <141748634+razanalkindi@users.noreply.github.com>
2023-08-21 18:55:11 +0400

    Add files via upload

---
+++ session11/script.js
@@ -0,0 +1,14 @@
+
+// let img =document.querySelector('img')
+// let sourses = ['img/img1.jpg' , 'img/img2.jpg' , 'img/img4.jpg' ]
+// let x =0
+
+// setInterval( ()=>{
+//     x++
+//     img.setAttribute("src" , sourses[x]);
+//     if(x >sourses.length -1)
+//     {
+//         x =0;
+//     }
+
+// },2000)
